293fd0c3efa9d5be5b9072ebbf4475dbbae302b7
Pedro Bern <pedrobermoreira@gmail.com>
2020-01-22 18:19:15 -0300

    add verify account tests

---
+++ tests/test_verify_account.py
@@ -0,0 +1,69 @@
+from django.contrib.auth import get_user_model
+
+from .testCases import RelayTestCase, DefaultTestCase
+from graphql_auth.constants import Messages
+from graphql_auth.utils import get_token, get_token_paylod
+from graphql_auth.models import UserStatus
+
+
+class VerifyAccountCaseMixin:
+    def setUp(self):
+        self.user1 = self.register_user(
+            email="foo@email.com", username="foo", verified=False
+        )
+        self.user2 = self.register_user(
+            email="bar@email.com", username="bar", verified=True
+        )
+
+    def test_verify_user(self):
+        token = get_token(self.user1, "activation")
+        executed = self.make_request(self.verify_query(token))
+        self.assertEqual(executed["success"], True)
+        self.assertFalse(executed["errors"])
+
+    def test_verified_user(self):
+        token = get_token(self.user2, "activation")
+        executed = self.make_request(self.verify_query(token))
+        self.assertEqual(executed["success"], False)
+        self.assertEqual(
+            executed["errors"]["nonFieldErrors"], Messages.ALREADY_VERIFIED
+        )
+
+    def test_invalid_token(self):
+        executed = self.make_request(self.verify_query("faketoken"))
+        self.assertEqual(executed["success"], False)
+        self.assertEqual(
+            executed["errors"]["nonFieldErrors"], Messages.INVALID_TOKEN
+        )
+
+    def test_other_token(self):
+        token = get_token(self.user2, "password_reset")
+        executed = self.make_request(self.verify_query(token))
+        self.assertEqual(executed["success"], False)
+        self.assertEqual(
+            executed["errors"]["nonFieldErrors"], Messages.INVALID_TOKEN
+        )
+
+
+class VerifyAccountCase(VerifyAccountCaseMixin, DefaultTestCase):
+    def verify_query(self, token):
+        return """
+        mutation {
+            verifyAccount(token: "%s")
+                { success, errors }
+            }
+        """ % (
+            token
+        )
+
+
+class VerifyAccountRelayTestCase(VerifyAccountCaseMixin, RelayTestCase):
+    def verify_query(self, token):
+        return """
+        mutation {
+        verifyAccount(input:{ token: "%s"})
+            { success, errors  }
+        }
+        """ % (
+            token
+        )
