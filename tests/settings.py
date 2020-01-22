4e56bd26fe884c27d3775f45ede02466270ac98d
Pedro Bern <pedrobermoreira@gmail.com>
2020-01-22 15:05:44 -0300

    update settings

--- tests/settings.py
+++ tests/settings.py
@@ -11,6 +11,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.staticfiles",
+    "graphene_django",
     "graphql_jwt.refresh_token.apps.RefreshTokenConfig",
     "django_filters",
     "graphql_auth",
@@ -55,9 +56,29 @@ MIDDLEWARE = [
 
 
 GRAPHENE = {
-    "MIDDLEWARE": ["graphql_jwt.middleware.JSONWebTokenMiddleware"],
+    "SCHEMA": "schema.schema",
+    "MIDDLEWARE": ["graphql_jwt.middleware.JSONWebTokenMiddleware",],
 }
 
+AUTHENTICATION_BACKENDS = [
+    "graphql_auth.backends.GraphQLAuthBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 GRAPHQL_JWT = {
+    "JWT_VERIFY_EXPIRATION": True,
     "JWT_LONG_RUNNING_REFRESH_TOKEN": True,
+    "JWT_ALLOW_ANY_CLASSES": [
+        "graphql_auth.mutations.Register",
+        "graphql_auth.mutations.VerifyAccount",
+        "graphql_auth.mutations.ResendActivationEmail",
+        "graphql_auth.mutations.SendPasswordResetEmail",
+        "graphql_auth.mutations.PasswordReset",
+        "graphql_auth.mutations.ObtainJSONWebToken",
+        "graphql_auth.mutations.VerifyToken",
+        "graphql_auth.mutations.RefreshToken",
+        "graphql_auth.mutations.RevokeToken",
+    ],
 }
+
+GRAPHQL_AUTH = {}
