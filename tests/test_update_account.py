f03a78eba63c4d22cdac0d423b478ff989679d58
Pedro Bern <pedrobermoreira@gmail.com>
2020-01-22 18:00:37 -0300

    fix tests

--- tests/test_update_account.py
+++ tests/test_update_account.py
@@ -6,17 +6,12 @@ from graphql_auth.constants import Messages
 
 class UpdateAccountTestCaseMixin:
     def setUp(self):
-        self.user = get_user_model().objects.create(
-            email="foo@email.com", username="username", is_active=True,
+        self.user1 = self.register_user(
+            email="foo@email.com", username="foo", verified=False
         )
-        self.user.set_password("sdfsdfff3f")
-        self.user.save()
-
-        self.user2 = get_user_model().objects.create(
-            email="boo@email.com", username="username2", is_active=True,
+        self.user2 = self.register_user(
+            email="bar@email.com", username="bar", verified=True
         )
-        self.user2.set_password("sdfsdfff3f")
-        self.user2.save()
 
     def test_update_account_unauthenticated(self):
         executed = self.make_request(self.get_query())
@@ -25,44 +20,54 @@ class UpdateAccountTestCaseMixin:
             executed["errors"]["nonFieldErrors"], Messages.UNAUTHENTICATED,
         )
 
-    def test_update_account_duplicate_field(self):
-        variables = {"user": self.user}
-        executed = self.make_request(self.get_query("username2"), variables)
+    def test_update_account_not_verified(self):
+        variables = {"user": self.user1}
+        executed = self.make_request(self.get_query(), variables)
         self.assertEqual(executed["success"], False)
-        self.assertTrue(executed["errors"])
-        self.user.refresh_from_db()
-        self.assertEqual(self.user.username, "username")
+        self.assertEqual(
+            executed["errors"]["nonFieldErrors"], Messages.NOT_VERIFIED,
+        )
 
     def test_update_account(self):
-        variables = {"user": self.user}
+        variables = {"user": self.user2}
         executed = self.make_request(self.get_query(), variables)
         self.assertEqual(executed["success"], True)
         self.assertEqual(
             executed["errors"], None,
         )
-        self.user.refresh_from_db()
-        self.assertEqual(self.user.username, "updated_username")
+        self.user2.refresh_from_db()
+        self.assertEqual(self.user2.first_name, "firstname")
+
+    def test_invalid_form(self):
+        variables = {"user": self.user2}
+        executed = self.make_request(
+            self.get_query("longstringwithmorethan30characters"), variables
+        )
+        self.assertEqual(executed["success"], False)
+        self.assertTrue(executed["errors"]["firstName"])
+        self.user2.refresh_from_db()
+        self.assertEqual(self.user2.first_name, "")
 
 
 class UpdateAccountTestCase(UpdateAccountTestCaseMixin, DefaultTestCase):
-    def get_query(self, username="updated_username"):
+    def get_query(self, first_name="firstname"):
         return """
         mutation {
-            updateAccount(username: "%s")
+            updateAccount(firstName: "%s")
                 { success, errors  }
         }
         """ % (
-            username
+            first_name
         )
 
 
 class UpdateAccountRelayTestCase(UpdateAccountTestCaseMixin, RelayTestCase):
-    def get_query(self, username="updated_username"):
+    def get_query(self, first_name="firstname"):
         return """
         mutation {
-            updateAccount(input:{ username: "%s" })
+            updateAccount(input:{ firstName: "%s" })
                 { success, errors }
         }
         """ % (
-            username
+            first_name
         )
